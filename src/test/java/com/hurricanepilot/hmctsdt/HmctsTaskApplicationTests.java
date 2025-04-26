f08d8b7414cfe5e9916c8c139e1efeea19a011a6
hurricanepilot <hurricanepilot@gmail.com>
2025-04-26 12:22:04 +0100

    Added spring bootstrap verification test

--- src/test/java/com/hurricanepilot/hmctsdt/HmctsTaskApplicationTests.java
+++ src/test/java/com/hurricanepilot/hmctsdt/HmctsTaskApplicationTests.java
@@ -1,14 +1,22 @@
 package com.hurricanepilot.hmctsdt;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+
 class HmctsTaskApplicationTests {
 
-	@Test
-	void contextLoads() {
-		// 
-	}
-	
+    @Test
+    void ensureSpringApplicationIsBootstrapped() {
+        // we need to ensure this mock gets closed as testing for coverage
+        // in vscode leaves the mock bound, breaking subsequent tests
+        try (var springApp = mockStatic(SpringApplication.class)) {
+            var args = new String [] {""};
+            HmctsTaskApplication.main(args);
+            springApp.verify(() -> SpringApplication.run(HmctsTaskApplication.class, args), times(1));
+        }
+    }
+
 }
